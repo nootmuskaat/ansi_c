54db5345fa387f8c596abf1458735567183b4b8c
nootmuskaat <acr.pobox@gmail.com>
2018-01-11 17:57:06 +0100

    added checks to not set *pn if not isdigit(c)

--- ch5/ex01.c
+++ ch5/ex01.c
@@ -33,9 +33,13 @@ int getint(int *pn) {
     sign = (c == '-') ? -1 : 1;
     if (c == '+' || c == '-')
         c = getch();
-    for (*pn = 0; isdigit(c); c = getch())
-        *pn = 10 * *pn + (c - '0');
-    *pn *= sign;
+    if (!isdigit(c))
+        ungetch(c);
+    else {
+        for (*pn = 0; isdigit(c); c = getch())
+            *pn = 10 * *pn + (c - '0');
+        *pn *= sign;
+    }
     if (c != EOF)
         ungetch(c);
     return c;
