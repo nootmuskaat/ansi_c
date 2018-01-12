5ac1b3734e55f063018ab910ef737b2b4a3f116e
nootmuskaat <acr.pobox@gmail.com>
2018-01-12 22:26:27 +0100

    pointer version of strcat functional

--- ch5/ex03.c
+++ ch5/ex03.c
@@ -2,20 +2,36 @@
  * Write a pointer version of the function strcat that we showed in Chapter 2:
  * strcat(s,t) copies the string t to the end of s
  */
+#include <assert.h>
+#include <string.h>
 
-void orig_strcat(char s[], char t[]);
-void strcat(char *s, char *t);
+#define LEN 1000
+
+void new_strcat(char *s, char *t);
 int test_strcat(void);
 
 int main() {
-    
-}
-
-int test_strcat(void) {
+    test_strcat();
     return 0;
 }
 
-void strcat(char *s, char *t) {
+int test_strcat(void) {
+    char s[LEN] = "";
+    new_strcat(s, "This ");
+    new_strcat(s, "is ");
+    new_strcat(s, "just ");
+    new_strcat(s, "a ");
+    new_strcat(s, "test.");
+    assert(!strcmp(s, "This is just a test."));
+    return 0;
+}
+
+void new_strcat(char *s, char *t) {
+    while (*s)
+        s++;
+    while ((*s++ = *t++))
+        ;
+    return;
 }
 
 void orig_strcat(char s[], char t[]) {
