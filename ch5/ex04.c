00d115b608ef04737a9b592554a9927c321dd5ea
nootmuskaat <acr.pobox@gmail.com>
2018-01-13 18:39:33 +0100

    strend all green

--- ch5/ex04.c
+++ ch5/ex04.c
@@ -3,15 +3,24 @@
  * the end of the string s , and zero otherwise.
  **/
 #include <assert.h>
-#include <stdio.h>
-#include <string.h>
 
-int strend(char *s, char *t);
-int test_strend(void);
-
-int main() {
-    test_strend();
-    return 0;
+int strend(char *s, char *t) {
+    //int i, j;
+    char *ps = s;
+    char *pt = t;
+    while (*ps)
+        ps++;
+    while (*pt)
+        pt++;
+    while (*pt == *ps && pt >= t && ps >= s) {
+        pt--;
+        ps--;
+    }
+    // if the loop exited because all of t was found in s
+    if (pt < t)
+        return 1;
+    else
+        return 0;
 }
 
 int test_strend(void) { 
@@ -21,13 +30,12 @@ int test_strend(void) {
     assert(!strend("\n", "\t"));
     assert(strend("ok\n", "\n"));
     assert(strend("walk the dog", "dog"));
+    assert(strend("walk the dog", "walk the dog"));
     assert(!strend("walk the dog.", "dog"));
     return 0;
 }
 
-int strend(char *s, char *t) {
+int main() {
+    test_strend();
     return 0;
 }
-
-
-
